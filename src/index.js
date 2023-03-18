dff607403d0f706c5dd0716b969d7b4c8331474f
KaiqueSP96 <kaiqueba6@gmail.com>
2023-03-18 12:12:56 -0300

    add var colors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/generics/reset.css'
-
+import './styles/settings/colors.css';
+import './styles/generics/reset.css';
+import './styles/elements/base.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

4771767e4dfb41bb667dd4f44b521ad650aefebc
KaiqueSP96 <kaiqueba6@gmail.com>
2023-03-18 01:22:20 -0300

    add normalize reset css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World!
-        </a>
-      </header>
+      
+      <a href='#' className='action'>Sobre</a>
+      
     </div>
   );
 }
